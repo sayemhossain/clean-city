86fc711b5ff1c10a5a3fe664f4f33a816091ad83
SAYAM HOSSAIN <shayem15-12011@diu.edu.bd>
2022-08-13 00:37:29 +0600

    enw code adkflsjfjs

--- src/pages/Home/Branding/Branding.js
+++ src/pages/Home/Branding/Branding.js
@@ -8,7 +8,7 @@ const Branding = () => {
   return (
     <div className="md:px-28 md:my-10 max-w-[1440px] mx-auto">
       <div className="grid grid-cols-1 md:grid-cols-4 gap-5">
-        <div className="text-center shadow-lg rounded-xl p-4 bg-primary">
+        <div className="text-center shadow-lg rounded-xl p-4 bg-primary hover:shadow-md duration-300">
           <img className="w-14 mx-auto " src={photo4} alt="" />
           <h2 className="mt-5 mb-2 font-semibold text-secondary">
             Work Safely
@@ -18,7 +18,7 @@ const Branding = () => {
             refines.
           </p>
         </div>
-        <div className="text-center shadow-lg rounded-xl p-4 bg-[#F0FFFC]">
+        <div className="text-center shadow-lg rounded-xl p-4 bg-[#F0FFFC] hover:shadow-md duration-300">
           <img className="w-14 mx-auto" src={photo2} alt="" />
           <h2 className="mt-5 mb-2 font-semibold text-secondary">
             Moving Help
@@ -28,7 +28,7 @@ const Branding = () => {
             here!
           </p>
         </div>
-        <div className="text-center shadow-lg rounded-xl p-4 bg-[#F9F8FE]">
+        <div className="text-center shadow-lg rounded-xl p-4 bg-[#F9F8FE] hover:shadow-md duration-300">
           <img className="w-14 mx-auto" src={photo3} alt="" />
           <h2 className="mt-5 mb-2 font-semibold text-secondary">
             Waste Collection
@@ -37,7 +37,7 @@ const Branding = () => {
             We dispose most industrial item's waste appropriately and neatly.
           </p>
         </div>
-        <div className="text-center shadow-lg rounded-xl p-4 bg-[#faffcc]">
+        <div className="text-center shadow-lg rounded-xl p-4 bg-[#faffcc] hover:shadow-md duration-300">
           <img className="w-14 mx-auto" src={photo1} alt="" />
           <h2 className="mt-5 mb-2 font-semibold text-secondary">Recycling</h2>
           <p>
