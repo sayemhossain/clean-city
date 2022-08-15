9bccf24bc4a4e2c26ce09883cfd1e2d48c670e5c
SAYAM HOSSAIN <shayem15-12011@diu.edu.bd>
2022-08-15 22:20:26 +0600

    new code added

--- src/pages/Home/LatestNews/LatestNews.js
+++ src/pages/Home/LatestNews/LatestNews.js
@@ -8,7 +8,7 @@ const LatestNews = () => {
     <div className="md:px-28 my-10">
       <h1 className="text-3xl font-bold text-center">Latest News & Articles</h1>
       <div className="my-6">
-        <div className="grid grid-cols-1 md:grid-cols-3 gap-5">
+        <div className="grid grid-cols-1 md:grid-cols-3 gap-5 px-4">
           {blogs.map((blog) => (
             <LatestNewsContainer
               key={blog._id}
--- src/pages/Home/LatestNews/LatestNewsContainer.js
+++ src/pages/Home/LatestNews/LatestNewsContainer.js
@@ -1,11 +1,11 @@
-import { faArrowRight } from "@fortawesome/free-solid-svg-icons";
+import { faAnglesRight, faArrowRight } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import React from "react";
 
 const LatestNewsContainer = ({ blog }) => {
   const { name, img, des, category } = blog;
   return (
-    <div className="shadow-lg p-5 rounded-lg">
+    <div className="shadow-lg p-5 rounded-lg hover:shadow-xl duration-300">
       <div>
         <div className="relative">
           <img className="rounded-md" src={img} alt="" />
@@ -13,12 +13,16 @@ const LatestNewsContainer = ({ blog }) => {
             {category}
           </p>
         </div>
-        <h1 className="mt-3 font-bold text-2xl text-gray-700">{name}</h1>
+        <h1 className="mt-3 font-bold text-xl text-gray-700">{name}</h1>
         <p className="mt-2">
           {des.length == 200 ? des : des.slice(0, 200) + "..."}
         </p>
-        <p className="mt-1 text-secondary">
-          Read More <FontAwesomeIcon icon={faArrowRight}></FontAwesomeIcon>
+        <p className="mt-1 text-secondary hover:cursor-pointer hover:underline ">
+          Read More{" "}
+          <FontAwesomeIcon
+            className="text-xs"
+            icon={faAnglesRight}
+          ></FontAwesomeIcon>
         </p>
       </div>
     </div>
