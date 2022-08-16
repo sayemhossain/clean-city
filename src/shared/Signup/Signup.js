22a350abc552ff4f900f36cd60bb754c8c7e9aef
SAYAM HOSSAIN <shayem15-12011@diu.edu.bd>
2022-08-17 01:53:01 +0600

    new code added

--- src/shared/Signup/Signup.js
+++ src/shared/Signup/Signup.js
@@ -1,18 +1,52 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import logo from "../../img/logo.svg";
 import googleLogo from "../../img/google.svg";
 import {
   useCreateUserWithEmailAndPassword,
-  useSignInWithFacebook,
   useSignInWithGoogle,
 } from "react-firebase-hooks/auth";
 import { auth } from "../../firebase.init";
+import { useUpdateProfile } from "react-firebase-hooks/auth";
+import useToken from "../../hooks/useToken";
 
 const Signup = () => {
+  const [createUserWithEmailAndPassword, user, error] =
+    useCreateUserWithEmailAndPassword(auth, { sendEmailVerification: true });
   const [signInWithGoogle, googleUser, googleLoading, googleError] =
     useSignInWithGoogle(auth);
+  const [updateProfile] = useUpdateProfile(auth);
+  const navigate = useNavigate();
 
+  const [token] = useToken(user || googleUser);
+  const handleSignup = async (e) => {
+    e.preventDefault();
+
+    // Read all input value from form for signup
+    const firstName = e.target.firstName.value;
+    const lastName = e.target.lastName.value;
+    const fullName = firstName + " " + lastName;
+    const email = e.target.email.value;
+    const password = e.target.password.value;
+    console.log(fullName);
+
+    // for update name
+    await updateProfile({ displayName: fullName });
+    await createUserWithEmailAndPassword(email, password);
+  };
+  let errorElement;
+  if (error || googleError) {
+    errorElement = (
+      <div>
+        <p>
+          <span className="text-primary ml-1">{error?.message}</span>
+        </p>
+      </div>
+    );
+  }
+  if (token) {
+    navigate("/dashboard");
+  }
   return (
     <div>
       <div className="flex justify-center items-center px-5 md:px-28 my-10">
@@ -26,7 +60,7 @@ const Signup = () => {
             </div>
             <div
               onClick={() => signInWithGoogle()}
-              className="w-full shadow-md rounded-md p-2.5 flex items-center justify-center gap-3"
+              className="w-full shadow-md rounded-md p-2.5 flex items-center justify-center gap-3 hover:cursor-pointer bg-gray-50 hover:shadow-sm duration-300"
             >
               <img src={googleLogo} alt="" />
               <p className="">Continue With Google</p>
@@ -34,30 +68,30 @@ const Signup = () => {
           </div>
           <div class="divider mt-2">OR</div>
           <div className="">
-            <form action="">
+            <form action="" onSubmit={handleSignup}>
               <div>
                 <div className="flex gap-2">
                   <div class="form-control">
                     <label class="label">
-                      <span class="label-text">Full Name</span>
+                      <span class="label-text">First Name</span>
                     </label>
                     <input
                       type="text"
-                      name="name"
+                      name="firstName"
                       required
-                      placeholder="Your Full Name"
+                      placeholder="Your First Name"
                       class="input input-bordered rounded-md w-full"
                     />
                   </div>
                   <div class="form-control">
                     <label class="label">
-                      <span class="label-text">Phone number</span>
+                      <span class="label-text">Last Name</span>
                     </label>
                     <input
                       type="text"
-                      name="number"
+                      name="lastName"
                       required
-                      placeholder="Your Phone number"
+                      placeholder="Your last Name"
                       class="input input-md rounded-md input-bordered w-full"
                     />
                   </div>
@@ -68,6 +102,7 @@ const Signup = () => {
                   </label>
                   <input
                     type="email"
+                    name="email"
                     required
                     placeholder="Your Email address"
                     class="input input-md rounded-md input-bordered w-full"
@@ -79,6 +114,7 @@ const Signup = () => {
                   </label>
                   <input
                     type="password"
+                    name="password"
                     placeholder="Your Password"
                     required
                     class="input input-md rounded-md input-bordered w-full"
