6a165385514b7082858db0b6344bbbe0842f0756
SAYAM HOSSAIN <shayem15-12011@diu.edu.bd>
2022-08-17 02:52:27 +0600

    login done

--- src/shared/Login/Login.js
+++ src/shared/Login/Login.js
@@ -1,9 +1,62 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import React, { useRef } from "react";
+import { Link, useNavigate } from "react-router-dom";
 import logo from "../../img/logo.svg";
 import googleLogo from "../../img/google.svg";
+import {
+  useSignInWithEmailAndPassword,
+  useSignInWithGoogle,
+} from "react-firebase-hooks/auth";
+import { auth } from "../../firebase.init";
+import useToken from "../../hooks/useToken";
+import { sendPasswordResetEmail } from "firebase/auth";
+import { toast, ToastContainer } from "react-toastify";
 
 const Login = () => {
+  const emailRef = useRef("");
+  const passwordRef = useRef("");
+  const [signInWithEmailAndPassword, user, loading, error] =
+    useSignInWithEmailAndPassword(auth);
+  const [signInWithGoogle, googleUser, googleLoading, googleError] =
+    useSignInWithGoogle(auth);
+  const navigate = useNavigate();
+
+  const [token] = useToken(user || googleUser);
+
+  const handleLogin = async (e) => {
+    e.preventDefault();
+
+    const email = emailRef.current.value;
+    const password = passwordRef.current.value;
+
+    await signInWithEmailAndPassword(email, password);
+  };
+
+  const resetPassword = () => {
+    const email = emailRef.current.value;
+    sendPasswordResetEmail(email);
+    toast("Reset email send!");
+  };
+
+  let errorElement;
+  if (error) {
+    errorElement = (
+      <div>
+        <p className="text-gray-800 text-xs mt-2 px-3">
+          User or password are not valid!
+          <span
+            onClick={resetPassword}
+            className="text-orange-400 ml-1 cursor-pointer underline"
+          >
+            Forget password?
+          </span>
+        </p>
+      </div>
+    );
+  }
+  if (token) {
+    navigate("/");
+  }
+
   return (
     <div>
       <div className="flex justify-center items-center px-5 md:px-28 my-10">
@@ -15,14 +68,17 @@ const Login = () => {
                 Welcome Back
               </h1>
             </div>
-            <div className="w-full shadow-md rounded-md p-2.5 flex items-center justify-center gap-3">
+            <div
+              onClick={() => signInWithGoogle()}
+              className="w-full shadow-md rounded-md p-2.5 flex items-center justify-center gap-3  hover:cursor-pointer bg-gray-50 hover:shadow-sm duration-300"
+            >
               <img src={googleLogo} alt="" />
               <p className="">Continue With Google</p>
             </div>
           </div>
           <div class="divider mt-2">OR</div>
           <div className="">
-            <form action="">
+            <form action="" onSubmit={handleLogin}>
               <div>
                 <div class="form-control">
                   <label class="label">
@@ -30,6 +86,7 @@ const Login = () => {
                   </label>
                   <input
                     type="email"
+                    ref={emailRef}
                     required
                     placeholder="Your Email address"
                     class="input input-md rounded-md input-bordered w-full"
@@ -41,13 +98,18 @@ const Login = () => {
                   </label>
                   <input
                     type="password"
+                    ref={passwordRef}
                     placeholder="Your Password"
                     required
                     class="input input-md rounded-md input-bordered w-full"
                   />
                 </div>
+                {errorElement}
                 <div>
-                  <button className="btn w-full mt-5 rounded-md btn-secondary text-white">
+                  <button
+                    type="submit"
+                    className="btn w-full mt-5 rounded-md btn-secondary text-white"
+                  >
                     Sign In
                   </button>
                 </div>
@@ -64,6 +126,7 @@ const Login = () => {
           </div>
         </div>
       </div>
+      <ToastContainer></ToastContainer>
     </div>
   );
 };
