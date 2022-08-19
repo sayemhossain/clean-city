950e9d7e9901ae6e37c03d92c35a52951b2d802f
SAYAM HOSSAIN <shayem15-12011@diu.edu.bd>
2022-08-19 23:24:07 +0600

    new code

--- src/pages/Home/LatestNews/LatestNewsContainer.js
+++ src/pages/Home/LatestNews/LatestNewsContainer.js
@@ -15,7 +15,7 @@ const LatestNewsContainer = ({ blog }) => {
     <div className="shadow-lg p-5 rounded-lg hover:shadow-xl duration-300">
       <div>
         <div className="relative">
-          <img className="rounded-md" src={img} alt="" />
+          <img className="rounded-md h-60 w-full" src={img} alt="" />
         </div>
         <h1 className="mt-3 font-bold text-xl text-gray-700">{name}</h1>
         <p className="mt-2">
