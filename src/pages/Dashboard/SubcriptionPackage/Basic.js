1bd39f361032ecafa4f9530f167b062eb1b76c55
SAYAM HOSSAIN <shayem15-12011@diu.edu.bd>
2022-09-09 02:28:50 +0600

    new code added

--- src/pages/Dashboard/SubcriptionPackage/Basic.js
+++ src/pages/Dashboard/SubcriptionPackage/Basic.js
@@ -1,5 +1,4 @@
 import React from "react";
-import Payment from "../Payment/Payment";
 
 const Basic = () => {
   return (
@@ -11,6 +10,14 @@ const Basic = () => {
         Basic Subcribtion
       </h3>
       <hr />
+      <div class="drawer drawer-mobile pb-10  bg-primary">
+        <input id="my-drawer-2" type="checkbox" class="drawer-toggle" />
+        <div class="drawer-content flex flex-col items-center mt-5">
+          <div>
+            <div></div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
