37fd9510a1bbd52b7ea1b3b34ab239d64870b5ed
SAYAM HOSSAIN <shayem15-12011@diu.edu.bd>
2022-09-09 02:53:32 +0600

    new code

--- src/pages/Dashboard/SubcriptionPackage/Basic.js
+++ src/pages/Dashboard/SubcriptionPackage/Basic.js
@@ -4,6 +4,7 @@ import { auth } from "../../../firebase.init";
 
 const Basic = () => {
   const [user] = useAuthState(auth);
+  const nameOfMonth = new Date().toLocaleString("default", { month: "long" });
   return (
     <div>
       <h3
@@ -25,14 +26,23 @@ const Basic = () => {
             </div>
             <div className="bg-gray-100 p-10">
               <form action="">
-                <div className="grid grid-cols-1 md:grid-cols-2 gap-x-2 gap-y-3">
-                  <input
-                    type="text"
-                    value={user.displayName}
-                    disabled
-                    placeholder="Type here"
-                    className="input w-full rounded-none"
-                  />
+                <div className="grid grid-cols-1 md:grid-cols-1 gap-x-2 gap-y-3">
+                  <div className="grid grid-cols-1 md:grid-cols-2 gap-2">
+                    <input
+                      type="text"
+                      value={user.displayName}
+                      disabled
+                      placeholder="Type here"
+                      className="input w-full rounded-none"
+                    />
+                    <input
+                      type="text"
+                      value="Basic"
+                      disabled
+                      placeholder="Type here"
+                      className="input w-full rounded-none"
+                    />
+                  </div>
                   <input
                     type="text"
                     value={user.email}
@@ -40,18 +50,25 @@ const Basic = () => {
                     placeholder="Type here"
                     className="input w-full rounded-none"
                   />
-                  <input
-                    type="text"
-                    value="Basic"
-                    disabled
-                    placeholder="Type here"
-                    className="input w-full rounded-none"
-                  />
-                  <input
-                    type="text"
-                    placeholder="Type here"
-                    className="input w-full rounded-none"
-                  />
+
+                  <div className="grid grid-cols-1 md:grid-cols-2 gap-2">
+                    <input
+                      type="text"
+                      value={nameOfMonth}
+                      disabled
+                      placeholder="Phone Number"
+                      className="input w-full rounded-none"
+                    />
+                    <input
+                      type="text"
+                      name="phone"
+                      placeholder="Phone Number"
+                      className="input w-full rounded-none"
+                    />
+                  </div>
+                  <button className="btn btn-secondary w-full rounded-none">
+                    Subcribe
+                  </button>
                 </div>
               </form>
             </div>
