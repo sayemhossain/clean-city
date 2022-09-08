a8532a4e7e0d750db07732cffa370879db85da7c
SAYAM HOSSAIN <shayem15-12011@diu.edu.bd>
2022-09-09 02:28:25 +0600

    new

--- src/pages/Dashboard/Payment/Payment.js
+++ src/pages/Dashboard/Payment/Payment.js
@@ -1,19 +1,13 @@
 import React, { useEffect, useState } from "react";
 import { useAuthState } from "react-firebase-hooks/auth";
 import { Link, Outlet, useParams } from "react-router-dom";
-import auth from "../../../firebase.init";
+import { auth } from "../../../firebase.init";
 import CustomLink from "../../../shared/CustomLink/CustomLink";
 
 const Payment = () => {
   const { id } = useParams();
   const [user] = useAuthState(auth);
 
-  const [order, setOrder] = useState({});
-  useEffect(() => {
-    fetch(`https://stark-shelf-45913.herokuapp.com/order/${id}`)
-      .then((res) => res.json())
-      .then((data) => setOrder(data));
-  }, []);
   return (
     <div className=" md:px-20 pb-20 bg-base-100">
       <div className="text-center py-5">
@@ -25,24 +19,7 @@ const Payment = () => {
         </h1>
       </div>
       <div className="card bg-gray-500 text-gray-200 mb-3 rounded-none ">
-        <div class=" p-2 flex items-center">
-          <div>
-            <img className="w-36 mr-5" src={order.img} alt="" />
-          </div>
-          <div>
-            <h2>
-              <span className="text-sm">
-                Hello, {order.customerNamer}
-                <br></br>
-              </span>{" "}
-              <span className="font-bold">
-                Please pay for : {order.productName}
-              </span>
-            </h2>
-            <p>Order quantity : {order.orderQuantity}</p>
-            <p>Total cost : {order.totalCost} tk</p>
-          </div>
-        </div>
+        <div class=" p-2 flex items-center"></div>
       </div>
       <div className="mt-5">
         <nav className="text-center mb-10 flex justify-center">
--- src/pages/Dashboard/SubcriptionPackage/Basic.js
+++ src/pages/Dashboard/SubcriptionPackage/Basic.js
@@ -1,4 +1,5 @@
 import React from "react";
+import Payment from "../Payment/Payment";
 
 const Basic = () => {
   return (
@@ -10,10 +11,6 @@ const Basic = () => {
         Basic Subcribtion
       </h3>
       <hr />
-      <div class="drawer drawer-mobile pb-10  bg-primary">
-        <input id="my-drawer-2" type="checkbox" class="drawer-toggle" />
-        <div class="drawer-content flex flex-col items-center mt-5"></div>
-      </div>
     </div>
   );
 };
